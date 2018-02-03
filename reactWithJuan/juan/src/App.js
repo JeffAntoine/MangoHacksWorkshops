24283f19e77b8680915e38e2b3c85c12ecacfad6
JeffAntoine <Janto031@fiu.edu>
2018-02-03 03:13:12 -0500

    tweet stuff looks cool

--- reactWithJuan/juan/src/App.js
+++ reactWithJuan/juan/src/App.js
@@ -2,11 +2,41 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
+var TWEET_LIMIT = 60;
+
+
 class App extends Component {
+
+  state = {
+    myTweet: ""
+  }
+
+  
+handleClick = (clickEvent) =>{
+  alert(this.state.myTweet);
+}
+
+
+  handleChange = (changeEvent) => {
+    //console.log(changeEvent);
+    this.setState({
+      myTweet: changeEvent.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
-       <textarea value = "my tweet" />
+      <h1>{this.state.myTweet}</h1>
+       <textarea
+       placeholder = "whats on your mind"       
+       value = {this.state.myTweet}
+       onChange= {this.handleChange}/>
+        <br />
+        <span>{TWEET_LIMIT - this.state.myTweet.length}</span> 
+        <br />
+        {this.state.myTweet.length >0? "show button": ""}
+        <button onClick= {this.handleClick}>Tweet</button>: ""}
       </div>
     );
   }
